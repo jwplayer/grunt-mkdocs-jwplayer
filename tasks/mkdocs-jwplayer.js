610569fb6273be84379de7c7225e6f06248cf5e6
Matthew Martindale <mmartindale@jwplayer.com>
2016-04-14 18:20:06 -0400

    debug

--- tasks/mkdocs-jwplayer.js
+++ tasks/mkdocs-jwplayer.js
@@ -80,9 +80,9 @@ module.exports = function(grunt) {
 
     if (options.disable.indexOf('run-http-server') == -1) {
       // run localhost server
-      if (shelljs.exec('node_modules/http-server/bin/http-server ' + config['siteDir'] + ' -p 8282 -a 127.0.0.1', function() {
+      shelljs.exec('node_modules/http-server/bin/http-server ' + config['siteDir'] + ' -p 8282 -a 127.0.0.1', function() {
         grunt.log.writeln('worked');
-      }));
+      });
       // listen for modified files that trigger rebuild while serving localhost
       grunt.log.writeln('watch files here');
     }
