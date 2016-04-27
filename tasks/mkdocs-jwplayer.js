ad6c8807b21f571f9b8a50c09d8ae867ae1a83f8
Matthew Martindale <mmartindale@jwplayer.com>
2016-04-27 18:03:23 -0400

    fix issue with server root dir

--- tasks/mkdocs-jwplayer.js
+++ tasks/mkdocs-jwplayer.js
@@ -66,6 +66,7 @@ module.exports = function(grunt) {
   grunt.registerTask('get-mkdocs-yaml-config', function() {
     if (!grunt.config('plugin.isSource')) {
       var mkdocsYml = yamljs.load('mkdocs.yml');
+      grunt.config('plugin.serve.root', mkdocsYml.site_dir || grunt.config('plugin.site_dir'));
       grunt.config('plugin.siteDir', mkdocsYml.site_dir || grunt.config('plugin.siteDir'));
       grunt.config('plugin.docsDir', mkdocsYml.docs_dir || grunt.config('plugin.docsDir'));
     }
