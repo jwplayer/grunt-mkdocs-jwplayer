b6729fcd6ab2eb2049031658524fa7b21ea94e98
Matthew Martindale <mmartindale@jwplayer.com>
2016-05-17 15:39:03 -0400

    test

--- tasks/mkdocs-jwplayer.js
+++ tasks/mkdocs-jwplayer.js
@@ -215,7 +215,9 @@ module.exports = function(grunt) {
   // local build process for the JW Player's custom MkDocs theme "mkdocs-jwplayer"
   grunt.registerMultiTask('mkdocs-jwplayer', function() {
 
-    shh.ok('!!!!!TEST!!!!!'['yellow']);
+    shh.unmute();
+    shh.mute();
+    shh.ok('!!!!!TEST!!!!!');
 
     // merge plugin config with any defined task options
     grunt.config('plugin', objectMerge(grunt.config('plugin'), this.options()));
